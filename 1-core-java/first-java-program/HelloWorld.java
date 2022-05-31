409053367740fe449870be5d6de6be513e8dc1e9
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-05-31 15:39:39 -0400

    add first-java-program & first-java-project

---
+++ 1-core-java/first-java-program/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        for (int i=0; i < args.length; i++) {
+             
+            System.out.println(args[i]);
+
+        }
+
+    }
+
+}
---
+++ 1-core-java/first-java-project/src/com/revature/HelloWorld.java
@@ -0,0 +1,43 @@
+package com.revature;
+
+/**
+ * A package in Java is used to group related classes. Think of it as a folder 
+ * in a file directory. We use packages to avoid name conflicts, 
+ * and to write a better maintainable code. 
+ * Packages are divided into two categories:
+ * 
+ * + Built-in Packages (packages from the Java API)
+ * + User-defined Packages (create your own packages)
+ */
+
+
+// This is a single line comment! The compiler won't pick up on it
+
+/*
+ * This is a multi-line comment - great for documentation!
+ * ctrl + makes it larger, ctrl - makes text smaller
+ */
+public class HelloWorld {
+	
+	// main + ctrl + space, enter
+	/**
+	 * In any Java Program the main() method is the entry point from where the
+	 * compiler starts program execution.
+	 * 
+	 * @param args ... represents commands that could follow the class name when we
+	 *             call the "java" execution command to execute the bytecode.
+	 * 
+	 *             We will talk later about what it means to be "public", "static",
+	 *             and "void"....
+	 */
+	public static void main(String[] args) {
+		
+		// sysout + ctrl + space....
+		System.out.println("Hello World from the IDE!");
+		
+		// alt  + shift + x, j (for windows!)
+		
+		// MEET BACK AT 3:45PM et 
+	}
+
+}
