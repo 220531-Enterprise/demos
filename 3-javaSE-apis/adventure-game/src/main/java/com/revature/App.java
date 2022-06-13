13871379283acd90f1a77828039e632ec41d6dec
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-06-13 11:31:25 -0400

    complete adventure game

--- 3-javaSE-apis/adventure-game/src/main/java/com/revature/App.java
+++ 3-javaSE-apis/adventure-game/src/main/java/com/revature/App.java
@@ -49,6 +49,63 @@ public class App {
 		locations.put(2, hill);
 		
 		// Put in the Building, Valley, and Forest locations
-	}
+		tempExits = new HashMap<String, Integer>();
+		tempExits.put("W", 1);
+		locations.put(3, new Location(3, "You're in an old brick building", tempExits));
 
+		tempExits = new HashMap<String, Integer>();
+		tempExits.put("N", 1);
+		tempExits.put("W", 2);
+		locations.put(4, new Location(4, "You're standing in the middle of a big green valley", tempExits));
+		
+		tempExits = new HashMap<String, Integer>();
+		tempExits.put("S", 1);
+		tempExits.put("W", 2);
+		locations.put(5, new Location(5, "You're in a dark forest", tempExits));
+		
+		// by this point all locations have been established
+		
+		// open up a while(true) loop to never end unless the user presses Q to Quit
+		
+		int loc = 1; // starting at the road
+		
+		while (true) {
+			
+			// print the current location's description
+			System.out.println(locations.get(loc).getDescription());
+			
+			// if the location is 0, break the loop
+			if (loc == 0) {
+				break;
+			}
+			
+			// list the available exits of the current location
+			System.out.println("The available exits are: ");
+			
+			Map<String, Integer> exits = locations.get(loc).getExits();
+			
+			for (String exit : exits.keySet()) {
+				
+				System.out.print(exit + ", "); // {N,  E, S, W }
+				 
+			}
+			
+			// prompt whert yould you like to go
+			System.out.println("Where would you like to go?");
+			
+			// capture the input
+			String direction = scan.next().toUpperCase(); // in the case they put in a lowercase letter
+			
+			// change the location based on the direction the user wants to go
+			if (exits.containsKey(direction)) {
+				
+				// reset the location equal to the value of that key
+				loc = exits.get(direction); 
+			} else {
+				
+				System.out.println("That's not a possible exit...");
+				// otherwise tell them that's not a possible direction, loop again
+			}		
+		}
+	}
 }
