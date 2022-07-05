b8bfbbf6a8d525a32a1f979538fae48913d6500d
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-07-05 13:03:29 -0400

    add start to xml bean wiring example

---
+++ 6-spring/spring-intro-xml-config/src/main/java/com/revature/SpringDriver.java
@@ -0,0 +1,19 @@
+package com.revature;
+
+import com.revature.models.BaseballCoach;
+import com.revature.services.MotivationService;
+
+public class SpringDriver {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Creating bean container!");
+		
+		// Let's use Spring ApplicationContext to define the construction of this bean
+//		BaseballCoach coach = new BaseballCoach(new MotivationService());
+		
+		
+		
+	}
+
+}
---
+++ 6-spring/spring-intro-xml-config/src/main/java/com/revature/services/MotivationService.java
@@ -0,0 +1,15 @@
+package com.revature.services;
+
+public class MotivationService {
+
+	public MotivationService() {
+		System.out.println("MotivationService no-args constructor invoked!");
+	}
+	
+	public String getMotivation() { 
+		
+		return "Don't quit. Suffer now and live the rest of your life a champion";
+		
+	}
+	
+}
